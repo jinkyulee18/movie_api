ccf9a46e5a6fb5d51cd498253c3cd94ba4ad6c70
jinkyulee18 <jinkyulee224@gmail.com>
2022-10-30 21:56:52 -0700

    fix struture

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ const app = express();
 // a ‘log.txt’ file is created in root directory
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
+// app.get('/movies', (res, req) =>{
+//   res.status(200).json(movies);
+// })
+//
+// app.get('/movies/:title', (req, res) => {
+// const {title} = req.params;
+// const movie = movies.find( movie => movies.Title === title );
+// })
+//
+//   if (movie) {
+//     res.status(200).json(movie);
+//   } else {
+//     res.status(400).send('no such movie')}
+
 let topTenMovies = [
   {
     Title: 'City Lights',
@@ -51,21 +65,62 @@ let topTenMovies = [
   }
 ];
 
+//Get methods
+app.get('/movies', function(req, res){
+      res.send('List of all of the movies')
+});
+
+app.get('/movies/Genre', function(req, res){
+    res.send('description of movie genres')
+});
+
+app.get('/movies/Title', function(req, res){
+    res.send('Get single movies by title')
+});
+
+app.get('/movies/directors/name', function(req, res){
+  res.send('Get director by name')
+});
+
+app.get('/users', function(req, res){
+  res.send('Retrieve all users')
+});
+
+app.get('/users/name', function(req, res){
+  res.send('Retrieve all users by username')
+});
+
+//Post methods
+app.post('/users/Login', function(req, res){
+  res.send('let existing users log in')
+});
+
+app.post('/users/Register', function(req, res){
+  res.send('let new users register')
+});
+
+app.post('/users/Movies/MovieID', function(req, res){
+  res.send('let users add favorited movies')
+});
+
+//put method
+app.put('/users/Info', function(req, res){
+  res.send('let users update their info')
+});
+
+//delete method
+app.delete('/movie/MovieID', function(req, res){
+  res.send('delete movie from favorited lsit')
+});
+
+app.delete('/users/UserID', function(req, res){
+  res.send('delete existing user')
+});
+
 // setup the logger
 app.use(morgan('common', {stream: accessLogStream}));
   app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my app!');
-});
-
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
-});
-
-app.get('/movies', (req, res) => {
-  res.json(topTenMovies);
-});
 
 // setup for Error
   app.use((err, req, res, next) => {
